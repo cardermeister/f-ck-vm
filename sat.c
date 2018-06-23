0dd40256d0978b54297f9965aaac7b58d22589fd
Card <smeash1997@gmail.com>
2018-06-24 00:24:03 +0700

    zhopa

---
+++ sat.c
@@ -0,0 +1,215 @@
+#include "sat.h"
+
+//int sc_memory[100]; // массив памяти
+
+int str2sc_word(char *str, int *value)
+{
+	int pos = 0, plus = 1;
+	int n;
+	int byte1, byte2;
+
+	if (str[0] == '+') {
+		plus = 0;
+		pos = 1;
+	}
+	if (sscanf(str + pos, "%x", &n) != 1)
+		return -1;
+	byte1 = n & 0xFF;
+	byte2 = n >> 8;
+	if ((byte1 > 0x7F) || (byte2 > 0x7F))
+		return -1;
+	*value = byte1 | (byte2 << 7) | (plus << 14);
+	return 0;
+}
+
+int str2command(char *str)
+{
+	int ret;
+
+	if (strcmp(str, "READ") == 0)
+		ret = 0x10;
+	else if (strcmp(str, "WRITE") == 0)
+		ret = 0x11;
+	else if (strcmp(str, "LOAD") == 0)
+		ret = 0x20;
+	else if (strcmp(str, "STORE") == 0)
+		ret = 0x21;
+	else if (strcmp(str, "ADD") == 0)
+		ret = 0x30;
+	else if (strcmp(str, "SUB") == 0)
+		ret = 0x31;
+	else if (strcmp(str, "DIVIDE") == 0)
+		ret = 0x32;
+	else if (strcmp(str, "MUL") == 0)
+		ret = 0x33;
+	else if (strcmp(str, "JUMP") == 0)
+		ret = 0x40;
+	else if (strcmp(str, "JNEG") == 0)
+		ret = 0x41;
+	else if (strcmp(str, "JZ") == 0)
+		ret = 0x42;
+	else if (strcmp(str, "HALT") == 0)
+		ret = 0x43;
+	else if (strcmp(str, "JNP") == 0)
+		ret = 0x59;
+	else
+		ret = -1;
+	
+	return ret;
+}
+
+int pars_line(char *str, int *addr, int *value)
+{
+	char *ptr;
+	int operand, command;
+	int flag_assign = 0;
+	
+	ptr = strchr(str, ';');
+	if (ptr != NULL)  *ptr = '\0';
+	ptr = strchr(str, '\n');
+
+	if (ptr != NULL) *ptr = '\0';
+
+	ptr = strtok(str, " \t");
+
+	if (ptr == NULL) return EMPTY_STR;
+
+	if (sscanf(ptr, "%d", addr) != 1)
+	{
+		return ERR_ARG1;
+	}
+
+	if ((*addr < 0) || (*addr >= 100)) return ERR_ARG1;
+
+	ptr = strtok(NULL, " \t");
+
+	if (ptr == NULL) return ERR_FEW;
+	else
+		if (strcmp(ptr, "=") == 0)
+		{
+			flag_assign = 1;
+		}
+	else
+	{
+		command = str2command(ptr);
+		if (command == -1)
+			return ERR_ARG2;
+	}
+
+	ptr = strtok(NULL, " \t");
+
+	if (ptr == NULL)return ERR_FEW;
+
+	ptr = strtok(NULL, " \t");
+
+	if (ptr != NULL)return ERR_MANY;
+
+	if (!flag_assign)
+	{
+		printf("%d %d\n",command, operand);
+		sc_commandEncode(command, operand, value);
+		
+	}
+	
+	return 0;
+}
+
+void print_error(char *line, int line_cnt, int err)
+{
+	switch (err) {
+		case ERR_ARG1:
+			fprintf(stderr, "%d: first argument isn't a number\n", line_cnt);
+			break;
+		
+		case ERR_ARG2:
+			fprintf(stderr, "%d: unknown command\n", line_cnt);
+			break;
+		
+		case ERR_ARG3:
+			fprintf(stderr, "%d: third argument isn't a number\n", line_cnt);
+			break;
+		
+		case ERR_FEW:
+			fprintf(stderr, "%d: should be three aguments\n", line_cnt);
+			break;
+		
+		case ERR_MANY:
+			fprintf(stderr, "%d: too many arguments\n", line_cnt);
+			break;
+	}
+	fprintf(stderr, "%s", line);
+}
+
+int test_argv(char *argv[])
+{
+	char *ptr1, *ptr2;
+	
+	ptr1 = strrchr(argv[1], '.');
+	ptr2 = strrchr(argv[2], '.');
+	if ((strcmp(ptr1, ".o") != 0) || (strcmp(ptr2, ".sa") != 0))
+		return -1;
+	else
+		return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+    sc_memoryLoad("save.o");
+    
+	char buf[256], line[256];
+	char add_mem[100];
+	FILE *input;
+	int value, addr, line_cnt = 1;
+	int err;
+	int flag_err = 0;
+	
+	if (argc != 3) {
+		perror("Incorrect arguments!\n");
+		exit(1);
+	}
+	if (test_argv(argv) != 0) {
+		perror("Incorrect arguments!\n");
+		exit(1);
+	}
+
+	if ((input = fopen(argv[2], "rb")) == NULL) {
+		fprintf(stderr, "Cannot open file:%s", argv[2]);
+		exit(1);
+	}
+	
+	//memset(add_mem, 0, 100);
+	//memset(sc_memory, 0, 100 * sizeof(int));
+    //sc_memorySet(add_mem,100);
+    //sc_memorySet(sc_memory,100 * sizeof(int));
+	while (fgets(line, 256, input)) {
+		strcpy(buf, line);
+		err = pars_line(buf, &addr, &value);
+		if (err == 0) {
+			if (add_mem[addr] == 0) {
+			    //sc_memory[addr] = value;
+			//	int val2;
+			//	sc_memoryGet(addr,&val2);
+			//	printf("pred %d %d\n",addr,val2);
+                sc_memorySet(addr,9999);
+			//	sc_memoryGet(addr,&val2);
+			//	printf("post %d %d\n",addr,val2);
+			}
+			else {
+				fprintf(stderr, "%d: Command with %d address already exists\n%s",  line_cnt, addr, line);
+				flag_err = 1;	
+			}
+		}
+		else if (err < 0) {
+			print_error(line, line_cnt, err);
+			flag_err = 1;
+		}
+		line_cnt++;
+	}
+	if (flag_err == 0)
+	{
+		sc_memorySave("save.o");
+	}
+	fclose(input);
+	return 0;
+}
---
+++ sat.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "fatherhead.h"
+
+#define ERR_ARG1 -1
+#define ERR_ARG2 -2
+#define ERR_ARG3 -3
+#define ERR_FEW -4
+#define ERR_MANY -5
+#define EMPTY_STR 1
+
+int str2sc_word(char *str, int *value);
+int str2command(char *str);
+int pars_line(char *str, int *addr, int *value);
+void print_error(char *line, int line_cnt, int err);
+int test_argv(char *argv[]);
+int main(int argc, char *argv[]);
